ff4ec273534d6cbabed7cd9f64d3577c55977786
gabrielvm98 <64874245+gabrielvm98@users.noreply.github.com>
2021-05-11 21:44:47 -0500

    Update 02-Analisis-A.r

--- code/02-Analisis-A.r
+++ code/02-Analisis-A.r
@@ -2,23 +2,19 @@
 
 ## Leer el dataset y analizarlo
 
-hotelBooking <- read.csv("data/hotel_bookings_clean.csv",header = TRUE, stringsAsFactors = FALSE)
-str(hotelBooking)
-summary(hotelBooking)
-
-## Comprobamos que la columna que analizaremos no tenga valores NA
-
-hotelBooking$arrival_date_year[is.na(hotelBooking$arrival_date_year) == TRUE]
+hotelBooking <- read.csv("data/hotel_bookings_clean.csv",header = TRUE, stringsAsFactors = FALSE);
+#str(hotelBooking)
+#summary(hotelBooking)
 
 ## Visualizar la cantidad de registros que hay en cada año
 
-table(hotelBooking$arrival_date_year)
+table(hotelBooking$arrival_date_year);
 
 ## Crear una tabla del número de reservas por año 
 
-hotelBookingYeardf <- data.frame(Year = table(hotelBooking$arrival_date_year))
-View(hotelBookingYeardf)
+hotelBookingYeardf <- data.frame(Year = table(hotelBooking$arrival_date_year));
+View(hotelBookingYeardf);
 
 ## Visualizar la cantidad de reservas por año en un gráfico de barras
 
-barplot(hotelBookingYeardf$Year.Freq, main = "Número de reservas por año", names.arg = hotelBookingYeardf$Year.Var1, col = "blue")
+barplot(hotelBookingYeardf$Year.Freq, main = "Número de reservas por año", names.arg = hotelBookingYeardf$Year.Var1, col = "blue");
